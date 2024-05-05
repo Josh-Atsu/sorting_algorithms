77d5d467e1c9c11db3934010245fbbb888171be4
Joshua <atsujoshua67@gmail.com>
2024-05-04 20:37:01 -0700

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,19 +13,59 @@ void insertion_sort_list(listint_t **list)
 
 	if ((*list) == NULL || list == NULL || (*list)->next == NULL)
 		return;
-	head = (*list)->next;
-	node = (*list)->next;
+	head = (*list);
 	while (head != NULL)
 	{
+		node = head->next; /* always update head with this*/
 		ptr = head->prev;
-		while (ptr->prev != NULL)
+		while (ptr != NULL && head->n < ptr->n)
 		{
-			if (node->n < ptr->n)
-			{
-				ptr->next = node->next;
-				node->next->prev = ptr;
-				ptr = ptr->prev;
-			}
+			swap_nodes(&(*list), ptr, head);
+			print_list(*list);
+			ptr = head->prev;
 		}
+		head = node;
 	}
+}
+
+/**
+ * swap_nodes - swapes the nodes
+ * @list_head: link to the original node
+ * @ptr_1: link to the previous node
+ * @head_1: link to the next node
+ */
+
+void swap_nodes(listint_t **list_head, listint_t *ptr_1, listint_t *head_1)
+{
+	listint_t *temp;
+
+	if (ptr_1 == NULL || head_1 == NULL)
+		return;
+
+	/* if either a or b is the head node */
+	if (*list_head == ptr_1)
+		*list_head = head_1;
+	else if (*list_head == head_1)
+		*list_head = ptr_1;
+
+	/* adjust previous pointers*/
+	if (ptr_1->prev != NULL)
+		ptr_1->prev->next = head_1;
+	if (head_1->prev != NULL)
+		head_1->prev->next = ptr_1;
+
+	/* adjust next pointers*/
+	if (ptr_1->next != NULL)
+		ptr_1->next->prev = head_1;
+	if (head_1->next != NULL)
+		head_1->next->prev = ptr_1;
+
+	/* swap the positions of ptr_1 and head_1 */
+	temp = ptr_1->prev;
+	ptr_1->prev =  head_1->prev;
+	head_1->prev = temp;
+
+	temp = ptr_1->next;
+	ptr_1->next = head_1->next;
+	head_1->next = temp;
 }
