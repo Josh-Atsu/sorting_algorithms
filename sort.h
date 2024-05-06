bc251d3402f3dac93ccef6754e9ee7108a2b443f
Joshua <atsujoshua67@gmail.com>
2024-05-06 03:39:15 -0700

    counting sort atlgorithm

--- sort.h
+++ sort.h
@@ -23,6 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap_nodes(listint_t **list_head, listint_t *ptr_1, listint_t *head_1);
+void counting_sort(int *array, size_t size);
 
 
 #endif /* sort.h */
